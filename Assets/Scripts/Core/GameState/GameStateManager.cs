3c318ef28774d3aff7ef96ef4a7d14a9fe9fed84
Luciano Claudio <lucianoclaudio.se@gmail.com>
2026-09-02 08:31:46 -0300

    feat: game state manager with real states (menu, select, gameplay, results, shop, gameover)

--- Assets/Scripts/Core/GameState/GameStateManager.cs
+++ Assets/Scripts/Core/GameState/GameStateManager.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
-public enum GameState { Gameplay, Paused }
+public enum GameState
+{
+    MainMenu,
+    ModeSelect,
+    HeroSelect,
+    MapSelect,
+    Gameplay,
+    Paused,
+    Results,
+    Shop,
+    GameOver
+}
 
 public class GameStateManager : MonoBehaviour
 {
     public static GameStateManager Instance { get; private set; }
-    public GameState CurrentState { get; private set; } = GameState.Gameplay;
+    public GameState CurrentState { get; private set; } = GameState.MainMenu;
 
     private void Awake() => Instance = this;
     public void SetState(GameState state) => CurrentState = state;
